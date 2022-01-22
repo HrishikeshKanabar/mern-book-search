a1aa831eb5fee63d2a4f8ab76d60f18414b1e925
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2022-01-22 18:01:41 -0500

    Refactoring code for resolvers and typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,14 @@
-const { User } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-   
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select("-__v -password")
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
 
         return userData;
       }
@@ -17,13 +17,12 @@ const resolvers = {
     },
   },
   Mutation: {
-  
     addUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
       return { token, user };
     },
- 
+
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
@@ -40,7 +39,7 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    
+
     saveBook: async (parent, args, context) => {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
@@ -54,7 +53,7 @@ const resolvers = {
 
       throw new AuthenticationError("You need to be logged in!");
     },
-   
+
     removeBook: async (parent, args, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
